c3da237e99dd728d78e2ae25614f03d76679a321
Alisson Rodrigues <alissonrodrigues565656@gmail.com>
2023-10-01 13:48:46 -0300

    feat: exception handler

---
+++ src/config/exception-handler.config.ts
@@ -0,0 +1,19 @@
+import { Catch, ExceptionFilter, HttpException, ArgumentsHost } from '@nestjs/common';
+
+@Catch()
+export class ExceptionHandler implements ExceptionFilter {
+  catch(exception: Error, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const status = exception instanceof HttpException ? exception.getStatus() : 500;
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message: exception.message,
+    });
+  }
+}
