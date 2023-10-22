61f6e164cabe96ed3de21787fcbeffc215052bac
Alisson Rodrigues <alissonrodrigues565656@gmail.com>
2023-10-22 16:55:21 -0300

    chore: prefix

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.setGlobalPrefix("strong-nutrition");
+  app.setGlobalPrefix("strong-nutrition/api");
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
